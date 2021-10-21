a060f4932c7ebfbc69d1e3ee6f17fca7b068b0c1
CrossEvol <CrossEvol@gmail.com>
2021-10-21 20:13:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author omf
+ * @create 2021-10-21 20:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
